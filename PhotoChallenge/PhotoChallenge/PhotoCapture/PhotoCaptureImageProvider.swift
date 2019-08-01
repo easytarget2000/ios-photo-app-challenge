01779471a46781ce353778b93d975f7f5cf45b9b
Michel Sievers <michel@easy-target.eu>
2019-08-02 00:02:42 +0200

    Populated PhotoCaptureImageProvider. Strings cleanup.

---
+++ PhotoChallenge/PhotoChallenge/PhotoCapture/PhotoCaptureImageProvider.swift
@@ -0,0 +1,59 @@
+import UIKit
+
+class PhotoCaptureImageProvider: NSObject {
+    
+    var imagePickerController = UIImagePickerController()
+    var callback: PhotoCaptureImageProviderCallback?
+    
+    func capturePhoto(callback: @escaping PhotoCaptureImageProviderCallback) {
+        self.callback = callback
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            handleNoImageSelection()
+            return
+        }
+        
+        imagePickerController.delegate = self
+    }
+    
+}
+
+// MARK: - UIImagePickerControllerDelegate
+
+extension PhotoCaptureImageProvider:
+UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
+    
+    func imagePickerController(
+        _ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info:
+            [UIImagePickerController.InfoKey : Any]
+    ) {
+        picker.dismiss(animated: true, completion: nil)
+
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            handleNoImageSelection()
+            return
+        }
+        
+        handleImageSelection(image: selectedImage)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        handleNoImageSelection()
+    }
+}
+
+// MARK: - Implementations
+
+extension PhotoCaptureImageProvider {
+    
+    fileprivate func handleImageSelection(image: UIImage) {
+        callback?(image)
+        callback = nil
+    }
+    
+    fileprivate func handleNoImageSelection() {
+        callback = nil
+    }
+    
+}
