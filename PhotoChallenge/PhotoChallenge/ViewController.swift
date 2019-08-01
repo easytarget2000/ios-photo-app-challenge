a25e6e4f254619733f49138406568f5652730fd1
Michel Sievers <michel@easy-target.eu>
2019-08-01 19:05:31 +0200

    Created "single screen" app with Xcode wizard.

---
+++ PhotoChallenge/PhotoChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoChallenge
+//
+//  Created by Michel Sievers on 01.08.19.
+//  Copyright © 2019 Easy Target. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
