69b9ed57cb600047478c9f9087956dcbc9027391
Michel Sievers <michel@easy-target.eu>
2019-08-05 19:46:46 +0200

    First connections between VM and VC.

--- PhotoChallenge/PhotoChallenge/PhotoGallery/PhotoGalleryTableViewController.swift
+++ PhotoChallenge/PhotoChallenge/PhotoGallery/PhotoGalleryTableViewController.swift
@@ -1,92 +1,68 @@
-//
-//  PhotoGalleryTableViewController.swift
-//  PhotoChallenge
-//
-//  Created by Michel Sievers on 05.08.19.
-//  Copyright © 2019 Easy Target. All rights reserved.
-//
-
 import UIKit
 
 class PhotoGalleryTableViewController: UITableViewController {
     
     @IBOutlet weak var viewModel: PhotoGalleryViewModel!
+    private var numberOfSections: Int {
+        get {
+            return viewModel.numberOfSections
+        }
+    }
+}
+
+// MARK: - UIViewController Life Cycle
+
+extension PhotoGalleryTableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        setupViewModel()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fetchElements()
+    }
+}
 
-    // MARK: - Table view data source
+// MARK: - UITableViewDataSource
+
+extension PhotoGalleryTableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return numberOfSections
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    override func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return numberOfRowsInSection(section)
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+}
 
-        // Configure the cell...
+// MARK: - Implementations
 
-        return cell
+extension PhotoGalleryTableViewController {
+    
+    private func setupViewModel() {
+        viewModel.elements.bind {
+            [weak self] in
+            let _ = $0
+            self?.reloadTableView()
+        }
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    private func fetchElements() {
+        viewModel.fetchElements()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    private func numberOfRowsInSection(_ section: Int) -> Int {
+        return viewModel.numberOfRowsInSection(section)
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    private func reloadTableView() {
+        tableView.reloadData()
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
