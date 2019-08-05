08c4a3872e05ad14b4c296474b27714f8056410e
Michel Sievers <michel@easy-target.eu>
2019-08-05 22:28:49 +0200

    Added PhotoGalleryElementCell reuse identifier.

--- PhotoChallenge/PhotoChallenge/PhotoGallery/PhotoGalleryElementCell.swift
+++ PhotoChallenge/PhotoChallenge/PhotoGallery/PhotoGalleryElementCell.swift
@@ -1,24 +1,7 @@
-//
-//  PhotoGalleryCell.swift
-//  PhotoChallenge
-//
-//  Created by Michel Sievers on 05.08.19.
-//  Copyright © 2019 Easy Target. All rights reserved.
-//
-
 import UIKit
 
 class PhotoGalleryElementCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    
+    static let identifier = "PhotoGalleryElementCell"
 
 }
