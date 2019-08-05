e262960c7d8c62c6d3e02a83a9a0e047387637e1
Michel Sievers <michel@easy-target.eu>
2019-08-05 19:48:11 +0200

    Moved showImagePickerControllerIfNeeded() to viewWillAppear().

--- PhotoChallenge/PhotoChallenge/PhotoCapture/PhotoCaptureTableViewController.swift
+++ PhotoChallenge/PhotoChallenge/PhotoCapture/PhotoCaptureTableViewController.swift
@@ -20,10 +20,6 @@ extension PhotoCaptureTableViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
         showImagePickerControllerIfNeeded()
     }
 }
